cef4e7a0890e8409e100e7a5b9b53eab492b1e46
maycry <byorickq@gmail.com>
2011-07-05 23:52:22 +0300

    Added Titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,11 +8,11 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have the right title" do
-       get 'home'
-       response.should.have_selector("title",
-                          :content => " | Home")
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -21,25 +21,26 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    
+
     it "should have the right title" do
-       get 'home'
-       response.should.have_selector("title",
-                          :content => " | Contact")
+      get 'contact'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-    
+
     it "should have the right title" do
-       get 'home'
-       response.should.have_selector("title",
-                          :content => " | About")
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | About")
     end
   end
- 
 end
